1a285d1bb16e79caea94c3c930a7ad0254b588f6
Josh Dudley <joshdudley.vt@gmail.com>
2020-04-01 21:00:58 -0400

    Update html, css, & js

--- script.js
+++ script.js
@@ -16,9 +16,25 @@ function addBookToLibrary(newbook) {
 
 function render() {
     for(let i = booksInLibrary; i < myLibrary.length; i++) {
-        let newCard = document.createElement('h1');
-        newCard.classList = 'newcard';
-        newCard.innerText = `${myLibrary[i].title} by ${myLibrary[i].author}, ${myLibrary[i].pages}, ${myLibrary[i].read}`;
+        const libraryTable = document.getElementById('library-table');
+        let newRow = libraryTable.insertRow(libraryTable.length);
+        let titleCell = newRow.insertCell(0);
+        titleCell.classList = 'title-cell';
+        titleCell.innerText = `${myLibrary[i].title}`;
+
+        let authorCell = newRow.insertCell(1);
+        authorCell.classList = 'author-cell';
+        authorCell.innerText = `${myLibrary[i].author}`;
+
+        let pagesCell = newRow.insertCell(2);
+        pagesCell.classList = 'pages-cell';
+        pagesCell.innerText = `${myLibrary[i].pages}`;
+
+        let statusCell = newRow.insertCell(3);
+        statusCell.classList = 'status-cell';
+
+        let deleteCell = newRow.insertCell(4);
+        deleteCell.classList = 'delete-cell';
 
         let readButton = document.createElement('button');
         readButton.classList = 'read-button-on';
@@ -34,18 +50,21 @@ function render() {
                 readButton.innerText = 'Read';
                 readButtonOn = true;
             }
-        })
+        });
 
         let removeButton = document.createElement('button');
         removeButton.classList = 'remove-button';
         removeButton.innerText = '+';
         removeButton.addEventListener('click', () => {
-                newCard.style.display = 'none';
+             titleCell.style.display = 'none';
+             authorCell.style.display = 'none';
+             pagesCell.style.display = 'none';
+             statusCell.style.display = 'none';
+             deleteCell.style.display = 'none';
         });
 
-        document.body.appendChild(newCard);
-        newCard.appendChild(readButton);
-        newCard.appendChild(removeButton);
+        statusCell.appendChild(readButton);
+        deleteCell.appendChild(removeButton);
     };
 };
 
@@ -86,7 +105,64 @@ newBookForm.addEventListener('submit', function(e) {
     };
 });
 
-/* let newTitle = document.createElement('h1');
+
+
+
+
+
+
+
+
+
+/* function render() {
+    for(let i = booksInLibrary; i < myLibrary.length; i++) {
+        let newCard = document.createElement('h1');
+        newCard.classList = 'newcard';
+        newCard.innerText = `${myLibrary[i].title} by ${myLibrary[i].author}, ${myLibrary[i].pages}, ${myLibrary[i].read}`;
+
+        let readButton = document.createElement('button');
+        readButton.classList = 'read-button-on';
+        readButton.innerText = 'Read';
+        readButton.addEventListener('click', () => {
+            if (readButtonOn) {
+                readButton.classList.remove('read-button-on')
+                readButton.classList = 'read-button-off';
+                readButton.innerText = 'Not read yet';
+                readButtonOn = false;
+            } else if (!readButtonOn){
+                readButton.classList.remove('read-button-off');
+                readButton.innerText = 'Read';
+                readButtonOn = true;
+            }
+        })
+
+        let removeButton = document.createElement('button');
+        removeButton.classList = 'remove-button';
+        removeButton.innerText = '+';
+        removeButton.addEventListener('click', () => {
+                newCard.style.display = 'none';
+        });
+
+        document.body.appendChild(newCard);
+        newCard.appendChild(readButton);
+        newCard.appendChild(removeButton);
+    };
+};
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+ let newTitle = document.createElement('h1');
         newTitle.innerText = this.title;
         let newAuthor = document.createElement('h1');
         newAuthor.innerText = this.author;
